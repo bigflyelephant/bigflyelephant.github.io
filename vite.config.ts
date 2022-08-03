833f804ad213479e83dfb4f17e477861fa4d1009
zqazqazqa <1679309912@qq.com>
2022-08-04 02:41:47 +0800

    feat: 清空原始文件内容

--- vite.config.ts
+++ vite.config.ts
@@ -2,29 +2,29 @@ import {defineConfig} from 'vite'
 import react from '@vitejs/plugin-react'
 import legacy from '@vitejs/plugin-legacy'
 import Compression from 'vite-compression-plugin';
-import { terser } from "rollup-plugin-terser";
+import {terser} from "rollup-plugin-terser";
 
 // https://vitejs.dev/config/
 export default defineConfig({
-    build:{
-        target:'es2015',
-        rollupOptions:{
-            plugins:[terser()]
+    build: {
+        target: 'es2015',
+        rollupOptions: {
+            plugins: [terser()]
+        }
+    },
+    css: {
+        modules: {
+            localsConvention: 'camelCaseOnly'
         }
     },
     plugins: [
         react(),
         legacy({
-            targets: ['defaults', '> 0.5%']
+            targets: ['defaults', '> 0.01%']
         }),
         Compression({
-            algorithm:'gzip'
-        }),
-        Compression({
-            algorithm:'deflateRaw'
-        }),
-        Compression({
-            algorithm:'brotliCompress'
+            algorithm: 'gzip',
+            loginfo:'silent'
         }),
     ],
 
