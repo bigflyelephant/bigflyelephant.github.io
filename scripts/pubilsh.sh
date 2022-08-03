a763d839fadd56884e1a1c531449cbb30f903369
zqazqazqa <1679309912@qq.com>
2022-08-04 01:49:47 +0800

    '删除dist'

--- scripts/pubilsh.sh
+++ scripts/pubilsh.sh
@@ -1,11 +1,9 @@
 yarn build
-
 cd ./dist
-
 git init
-git remote add origin git@github.com:bigflyelephant/bigflyelephant.github.io.git
-git branch -d gh-pages
+git branch gh-pages
 git checkout gh-pages
 git add .
 git commit -m "feat: 发版"
+git remote add origin git@github.com:bigflyelephant/bigflyelephant.github.io.git
 git push origin gh-pages -f
